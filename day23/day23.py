9fab2b25829f72f4c58aaa2c376f1e0574ba26f5
Emiel Steerneman <emielsteerneman@gmail.com>
2021-12-23 21:43:14 +0100

    day23

--- day23/day23.py
+++ day23/day23.py
@@ -1,152 +1,143 @@
+import functools
+
 power = {"A":1,"B":10,"C":100,"D":1000}
 
 ABCD = "ABCD"
+
+# example input 1
 rooms = ["BA", "CD", "BC", "DA"]
-spots = [" "*11]
+spots = "..........."
 
-rooms = ["AA", "..", "CC", "DD"]
-spots = "BB........."
+# example input 2
+rooms = ["BDDA", "CCBD", "BBAC", "DACA"]
+spots = "..........."
 
-rooms = ["AD", "BB", "CC", "DA"]
-spots = "BB........."
+# my input 1
+# rooms = ["DD", "CA", "BA", "CB"]
+# spots = "..........."
 
-rooms = ["AB", "DC", "CB", "AD"]
+# my input 2
+rooms = ["DDDD", "CCBA", "BBAA", "CACB"]
 spots = "..........."
 
 spots_idx = [0,1,3,5,7,9,10]
 
-# rooms = ["AA", "BB", "CC", "DD"]
-
-def print_room(rooms, spots):
-	string = "#############\n"
-	string += "#" + spots + "#\n"
-	string += "###" + "#".join([r[1] for r in rooms]) + "###\n"
-	string += "  #" + "#".join([r[0] for r in rooms]) + "#  \n"
-	string += "  #########  \n"
-	print(string)
-
-print_room(rooms, spots)
-
 def is_free(pathway, _from, _to, allow_self=False):
-	# print()
-	# print("[is_free]", _from, _to, allow_self)
-	
 	if _from < _to: _to += 1
 	if _to < _from: _from += 1
-	# print("[is_free]", _from, _to, allow_self)
+
 	if allow_self:
 		if _from < _to: _from += 1
 		if _to < _from: _from -= 1
-	# print("[is_free]", _from, _to, allow_self)
 
-	# print("[is_free]", pathway)
 	c,d = sorted([_from, _to])
 	r = list(range(c,d))
 
-	# print("[is_free]", "".join([ "^" if x in r else "-" for x in range(len(pathway)) ]))
-
 	pathway = pathway[c:d]
-	# print("pathway", pathway)
 	return pathway.count(".") == abs(c-d)
 
-# print(is_free("...A...", 0, 2))
-# print(is_free("...A...", 2, 0))
-# print(is_free("...A...", 0, 3, allow_self=True))
-# print(is_free("...A...", 3, 0, allow_self=True))
-# exit()
 
-
-def run(rooms, spots, total_power_consumed):
+@functools.cache
+def run(rooms, spots, total_power_consumed, max_power=2**31, depth=0):
+	rooms = eval(rooms)
 	rooms = rooms[:]
 	spots = spots[:]
 	
-	l = lambda *args: print("[run]", *args)
+	if max_power < total_power_consumed:
+		return total_power_consumed, [[rooms, spots, 0]]
 
-	if rooms == ["AA", "BB", "CC", "DD"]:
-		print("Done!")
-		return total_power_consumed
+	finished = True
+	for X, room in list(zip(ABCD, rooms)):
+		finished = finished and room.count(X) == len(room)
 
-	power_consumed = 0
+	if finished:
+		return total_power_consumed, [[rooms, spots, 0]]
 
-	all_powers_consumed = [2**31] 
+	minimal_power_consumed = max_power
+	minimal_state = [[rooms, spots]] 
+	min_power_required = 0
+
+	moved_in = False
 
 	# Check if X can move into correct room
-	l("======== MOVING IN")
 	for i in range(len(spots)):
 		X = spots[i]
 		if X == ".": continue
-
-		room = ABCD.index(X)
-		if rooms[room] != ".." and rooms[room] != X+".":
-			# l(X, rooms[room], "Room unavailable")
-			continue
-
-		l("Room available for", X)
-
-		room_entrance = (room+1)*2
+		room_idx = ABCD.index(X)
+		room = rooms[room_idx][:]
 		
-		if not is_free(spots, i, room_entrance, allow_self=True): 
-			l("Pathway blocked for", X)
+		# Check if X can move into room by checking if any !X is in it
+		if room.count(X) + room.count(".") != len(room):
 			continue
 
+		room_entrance = (room_idx+1)*2
+		
+		if not is_free(spots, i, room_entrance, allow_self=True):
+			continue
+
+		moved_in = True
 
 		# Move X to room
 		rooms_ = rooms[:]
 		spots_ = spots[:]
+				
+		power_required  = power[X] * abs(i-room_entrance)
+		power_required += power[X] * room.count(".")
 		
-		power_consumed = power[X]*abs(i-room_entrance)
-		if rooms_[room] == X + ".":
-			power_consumed += power[X]
-			rooms_[room] = X+X
-		if rooms_[room] == "..":
-			power_consumed += power[X]*2
-			rooms_[room] = X+"."
-		spots_ = spots_[:i] + "." + spots_[i+1:]
-		
-		print("power_consumed", power_consumed)
-		print_room(rooms_, spots_)
+		room_ = ("." * (room.count(".")-1) + X * len(room) )[:len(room)]
+		rooms_[room_idx] = room_
+		spots_ = spots[:i] + "." + spots[i+1:]
 
-		all_powers_consumed += [run(rooms_, spots_, total_power_consumed + power_consumed)]
+		power_consumed, state = run(str(rooms_), spots_, total_power_consumed + power_required, minimal_power_consumed, depth+1)
+		if power_consumed < minimal_power_consumed:
+			minimal_power_consumed = power_consumed
+			minimal_state = state[:]
+			minimal_state[-1] = power_required
+			min_power_required = power_required
 
-
-	l("======== MOVING OUT")
 	# Check if X can move out of room
 	for i_room, room in enumerate(rooms):
-		if room == "..": continue
-		if room == ABCD[i_room] + ".": continue
-		if room == ABCD[i_room] + ABCD[i_room]: continue
+		# If room is empty or correctly filled, skip
+		if room.count(".") + room.count(ABCD[i_room]) == len(room): 
+			continue
 
 		room_entrance = (i_room+1)*2
-		
-		room_idx = (room+".").index(".") - 1		
-		X = room[room_idx]
+		X_idx = room.count(".")
+		X = room[X_idx]
 
-		l(f"Considering moving {X} out from {room}")
 		for spot in spots_idx:
 			if not is_free(spots, room_entrance, spot): continue
-			print(" Can move to", spot)
 
 			spots_ = spots[:spot] + X + spots[spot+1:]
 			rooms_ = rooms[:]
-			rooms_[i_room] = room[::-1][room_idx]+"."
-			power_consumed = 0	
-			power_consumed += power[X] * (2-room_idx)
-			power_consumed += power[X] * abs(room_entrance - spot)
-			print("power_consumed", power_consumed)
-			print_room(rooms_, spots_)
+			room_ = room[:X_idx] + "." + room[X_idx+1:]
+			rooms_[i_room] = room_
+			
+			power_required = 0	
+			power_required += power[X] * (X_idx+1)
+			power_required += power[X] * abs(room_entrance - spot)
 
-			all_powers_consumed += [run(rooms_, spots_, total_power_consumed + power_consumed)]
+			power_consumed, state = run(str(rooms_), spots_, total_power_consumed + power_required, minimal_power_consumed, depth+1)
+			if power_consumed < minimal_power_consumed:
+				minimal_power_consumed = power_consumed
+				minimal_state = state[:]
+				minimal_state[-1] = power_required
+				min_power_required = power_required
 
-	print(all_powers_consumed)
-	l("<=", min(all_powers_consumed))
-	return min(all_powers_consumed)
-
-run(rooms, spots, 0)
-
-
-
+	return minimal_power_consumed, [[rooms, spots, min_power_required]] + minimal_state
 
 
+# my input 1
+rooms = ["DD", "CA", "BA", "CB"]
+spots = "..........."
+
+minimal_power_consumed, states = run(str(rooms), spots, 0, 2**31)
+print("Part 1:", minimal_power_consumed)
 
 
+# my input 2
+rooms = ["DDDD", "CCBA", "BBAA", "CACB"]
+spots = "..........."
 
+minimal_power_consumed, states = run(str(rooms), spots, 0, 2**31)
+print("Part 2:", minimal_power_consumed)
